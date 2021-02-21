be81e63803400918fbc8fb7480a1f77d867b0cef
DimonCool00 <smirdimon2000@mail.ru>
2021-02-21 18:23:33 +0600

    ProjectAWD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { IndexComponent } from './index/index.component';
+import { GaleryComponent } from './galery/galery.component';
+import { LoginComponent } from './login/login.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { RoomdetailComponent } from './roomdetail/roomdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    IndexComponent,
+    GaleryComponent,
+    LoginComponent,
+    RoomsComponent,
+    RegistrationComponent,
+    RoomdetailComponent
   ],
   imports: [
     BrowserModule,
